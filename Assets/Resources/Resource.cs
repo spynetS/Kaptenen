6b289a48760189cd5bb0eb86daf25a8dbfcb84ff
Alfred Roos <alfred@stensatter.se>
2025-03-26 14:22:18 +0100

    added resource class

---
+++ Assets/Resources/Resource.cs
@@ -0,0 +1,20 @@
+public class Resource : MonoBehavior
+{
+    public GameObject dropItem;
+    public int dropAmount;
+    public float health;
+
+    /**
+     *  Method to give damage to the Resource untill it dies and drop the drop items
+     *  */
+    public void TakeDamage(float damage){
+
+        if(this.health-- <= 0){
+            this.Die();
+        }
+    }
+    /** Drops the dropItem and destroyes it self */
+    private void Die(){
+
+    }
+}
