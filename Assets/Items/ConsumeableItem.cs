e62d4ffddb0ad279a22dba59049d60997dba12e4
Alfred Roos <alfred@stensatter.se>
2025-03-26 14:04:27 +0100

    update

--- Assets/Items/ConsumeableItem.cs
+++ Assets/Items/ConsumeableItem.cs
@@ -11,5 +11,4 @@ public class ConsumableItem : Item
     {
 
     }
-
 }
--- Assets/Items/Item.cs
+++ Assets/Items/Item.cs
@@ -5,7 +5,9 @@ using System.Collections.Generic;
  * Item is a object that can be picked up by a player
  * and used. This is mostly a abstract version to be implemented
  * but can be used to unsusable items.
- *  */
+ *
+ * How to upgrade
+ * *  */
 public class Item : MonoBehaviour
 {
     public GameObject nextUpgrade;
@@ -16,7 +18,7 @@ public class Item : MonoBehaviour
      *  */
     public virtual void Use()
     {
-        
+
     }
 
     public void Update(){
@@ -32,6 +34,9 @@ public class Item : MonoBehaviour
     public bool Upgrade(List<GameObject> cost){
         if(nextUpgrade){
             if(this.cost == (cost)){
+                foreach(GameObject v in cost){
+                    Destroy(cost);
+                }
                 GameObject newGameObject = Instantiate(nextUpgrade, transform.position, transform.rotation, gameObject.transform.parent);
                 Destroy(gameObject);
                 return true;
